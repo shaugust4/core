a50a73f60aff126fd6a887dd59a9803eee198748
seheon <osh8779@gmail.com>
2024-11-12 21:59:55 +0900

    spring_241102

--- src/main/java/hello/core/beanDefinition/BeanDefinitionTest.java
+++ src/main/java/hello/core/beanDefinition/BeanDefinitionTest.java
@@ -1,24 +1,25 @@
 package hello.core.beanDefinition;
-
 import hello.core.AppConfig;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+import org.springframework.context.support.GenericXmlApplicationContext;
 public class BeanDefinitionTest {
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext ac = new
+            AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
-    @DisplayName("빈 설정 메타 정보 확인")
-    public void findAppBean() {
+    @DisplayName("빈 설정 메타정보 확인")
+    public void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-
+            BeanDefinition beanDefinition =
+                    ac.getBeanDefinition(beanDefinitionName);
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
-                System.out.println("beanDefinitionName = " + beanDefinitionName +
-                        ", beanDefinition = " + beanDefinition);
+                System.out.println("beanDefinitionName" + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition);
             }
         }
     }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,20 +1,37 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+// 최근에는 생성자를 딱 1개 두고,
+// @Autowired 생략하는 방법을 주로 사용.
+// 여기에 Lombok 라이브러리의 @RequiredArgsConstructor와 함께 사용하면 기능은 다 제공되면서,
+// 코드를 깔끔하게 사용할 수 있다.
 public class OrderServiceImpl implements OrderService{
 
+    // 테스트 용
+    /*생성자 주입을 선택하는 이유
+    * 1. 불변
+    *  1) 대부분의 의존관계 주입은 한번 일어나면 애플리케이션 종료 시점까지 의존관계를 변경할 일이 없다.
+    * 오히려 대부분의 의존관계는 종료 전까지 변하면 안된다.
+    *  2) 수정자 주입을 사용하면, setXxx 메서드를 public으로 열어두어야 함.
+    *  누군가 실수로 변경할 수도 있고, 변경하면 안되는 메서드를 열어두는 것은 좋은 설계 방법이 아니다.
+    * 2. 누락
+    *  1) final 키워드를 사용해서 혹시라도 생성자에서 값이 누락되는 오류를 컴파일 시점에서 파악할 수 있다.
+    * <참고> 수정자 주입을 포함한 나머지 주입 방식은 생성자 이후에 호출되므로, 필드에 'final' 키워드를 사용할 수 없다.
+    **/
+    @Getter
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // 생성자가 1개라면 @Autowired 생략 가능
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -27,8 +44,6 @@ public class OrderServiceImpl implements OrderService{
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    // 테스트 용
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
 }
+
+
